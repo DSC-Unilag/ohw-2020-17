573e51a164853bb1eda6cdb87834045eaedf8372
Alade <alade.faruk11@gmail.com>
2020-07-31 06:38:01 +0100

    from faruk

---
+++ frontendReact/components/Gymdetails.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Gymdetails.css';
+
+const Gymdetails=()=>{
+  return(
+      <div>
+
+          <div  id='gydh' >
+          <div className='fl ml4 mt5  ' >
+          <h3 className=' white'>GYM NAME</h3> 
+          
+              
+         <p class="f6   white"><img src='' alt/>no 12 ####################################################</p>
+         <div class='white'>
+<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>-200 Reviews</span>
+</div>
+             </div>
+        
+          
+
+          <div className='fr mt4 mt2-m mr4  '>
+          <a className="f6 link dim br3  ba ph3  mb2 dtc 1 dib white bg-mid-gray" href="#0">share</a>
+          <a className="f6 link dim br3   pv2 ph4 mb2 dib dtc 1 white bg-hot-pink" href="#0">Book us</a>
+          </div>
+          </div>
+          
+      </div>
+  )
+}
+export default Gymdetails
---
+++ frontendReact/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+
+
+const Header=()=>{
+
+    return(
+        
+            <div>
+            <nav className="db dt-l w-100 border-box pa3 ph5-l">
+              <a className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" href="#" title="Home">
+                      <img src="images/logos/logo.png" className="" alt="logo"/>
+                   </a>
+                      <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
+                      <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Home">Home</a>
+                      <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="How it Works">How it Works</a>
+                      <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Blog">Blog</a>
+                        <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Press">Press</a>
+                       <a className="link dim dark-gray f6 f5-l dib" href="#" title="Contact">Contact</a>
+                        </div>
+                 </nav>
+  
+            
+        </div>
+    )
+}
+
+export default Header;
