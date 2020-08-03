45f4c12291e7d79cac1040f25da55594708fb163
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-08-03 09:15:41 +0100

    before pulling upstream version from deploy

--- src/contexts/userContext.js
+++ src/contexts/userContext.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import { userReducer } from "../reducers";
 import { loginUser, signOut } from "../actions";
-//import worker from "workerize-loader!../webWorkers/sessionChecker.js"; // eslint-disable-line import/no-webpack-loader-syntax
-
+import {  createBrowserHistory } from "history"
+import {withRouter } from "react-router-dom"
 var UserStateContext = React.createContext();
 var UserDispatchContext = React.createContext();
 
@@ -16,31 +16,8 @@ function UserProvider({ children }) {
     user: JSON.parse(localStorage.getItem("id_token")) || {},
   });
 
-  useEffect(() => {
-     sessionWorker.onmessage = handleSessionWorker;
-    //if the user is not logged in terminate the worker
-    if (!state.isAuthenticated) {
-      sessionWorker.terminate();
-    }
-    return () => {
-      sessionWorker.terminate();
-    };
-  }, [sessionWorker, state.isAuthenticated]);
-
-  function handleSessionWorker(event) {
-    if (event && event.data) {
-      //check if the session has expired
-
-      const { sessionExpired, message } = event.data;
-      if (message) {
-        console.log(message);
-      }
-      if (sessionExpired) {
-        //call signout action
-        signOut(dispatch);
-      }
-    }
-  }
+  
+  
   return (
     <UserStateContext.Provider value={state}>
       <UserDispatchContext.Provider value={dispatch}>
