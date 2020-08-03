299d3e36bb8fdf83c373a95bd34925e2e1f21b14
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-08-03 08:13:38 +0000

    remove webworker

--- src/contexts/userContext.js
+++ src/contexts/userContext.js
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react";
 import { userReducer } from "../reducers";
 import { loginUser, signOut } from "../actions";
-//import worker from "workerize-loader!../webWorkers/sessionChecker.js"; // eslint-disable-line import/no-webpack-loader-syntax
+import {  createBrowserHistory } from "history"
 
 var UserStateContext = React.createContext();
 var UserDispatchContext = React.createContext();
 
 function UserProvider({ children }) {
-  const sessionWorker = new Worker("/webWorkers/sessionChecker.js");
-
+  
   var [state, dispatch] = React.useReducer(userReducer, {
     isAuthenticated: !!localStorage.getItem("id_token"),
     isLoading: false,
@@ -16,32 +15,8 @@ function UserProvider({ children }) {
     user: JSON.parse(localStorage.getItem("id_token")) || {},
   });
 
-  useEffect(() => {
-     sessionWorker.onmessage = handleSessionWorker;
-    //if the user is not logged in terminate the worker
-    if (!state.isAuthenticated) {
-      sessionWorker.terminate();
-    }
-    return () => {
-      sessionWorker.terminate();
-    };
-  }, [sessionWorker, state.isAuthenticated]);
-
-  function handleSessionWorker(event) {
-    if (event && event.data) {
-      //check if the session has expired
-
-      const { sessionExpired, message } = event.data;
-      if (message) {
-        console.log(message);
-      }
-      if (sessionExpired) {
-        //call signout action
-        signOut(dispatch);
-      }
-    }
-  }
-  return (
+  
+   return (
     <UserStateContext.Provider value={state}>
       <UserDispatchContext.Provider value={dispatch}>
         {children}
